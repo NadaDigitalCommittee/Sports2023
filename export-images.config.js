3b828ec4771980b1f892432bc71c8c04cd4d08e8
altusershared <104960044+altusershared@users.noreply.github.com>
2023-09-27 10:39:58 +0900

    a

--- export-images.config.js
+++ export-images.config.js
@@ -3,8 +3,6 @@
  */
 const config = {
     basePath: "/2023",
-    filenameGenerator: ({ path, name, width, quality, extension }) =>
-        `${path.replace(/^\//, '').replace(/\//g, '-')}-${name}.${width}.${quality}.${extension}`,
 }
 
 module.exports = config
